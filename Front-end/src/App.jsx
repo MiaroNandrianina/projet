438f2f012f7c3827503dd660424885eeb2442a77
MiaroNandrianina <myaronandrianina@gmail.com>
2026-05-11 16:42:11 +0300

    refactor(app.jsx add code)

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,15 +1,16 @@
-import { BrowserRoute, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Public/Home";
+import Navbar from "./components/layouts/Navbar";
 function App() {
-
   return (
     <BrowserRouter>
-       <header></header>
+       <header><Navbar /></header>
 
-       <Route>
-         <Route path="" element={Accueil}></Route>
-       </Route>
+       <Routes>
+         <Route path="/" element={<Home />}></Route>
+       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;

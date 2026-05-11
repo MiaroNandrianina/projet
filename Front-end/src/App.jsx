e05799db23d6b477cef0029007090db256ae8c05
MiaroNandrianina <myaronandrianina@gmail.com>
2026-05-11 15:47:08 +0300

    feat(create router in app.jsx)

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,10 +1,14 @@
-
+import { BrowserRoute, Routes, Route } from "react-router-dom";
 function App() {
-  
+
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+       <header></header>
+
+       <Route>
+         <Route path="" element={Accueil}></Route>
+       </Route>
+    </BrowserRouter>
   )
 }
 

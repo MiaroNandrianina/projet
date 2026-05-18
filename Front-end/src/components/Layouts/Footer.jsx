429c587f66273cfd11554613b8114a44a5c026e6
MiaroNandrianina <myaronandrianina@gmail.com>
2026-05-18 16:05:52 +0300

    feat(create footer )

--- Front-end/src/components/Layouts/Footer.jsx
+++ Front-end/src/components/Layouts/Footer.jsx
@@ -1,42 +1,58 @@
+import { Link } from "react-router-dom";
+
+
 function Footer() {
     return (
-        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
+        <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10 no-print">
+
             <nav className="space-y-4">
-                <h6 className="footer-title">SUIVEZ NOUS</h6>
-                <div className="flex items-center gap-2">
-                    <i className="fa-brands fa-facebook-f text-2xl"></i>
-                    <a className="link link-hover">Facebook</a>
+                <h6 className="footer-title">SUIVEZ-NOUS</h6>
+                <div className="flex items-center gap-2 hover:text-primary transition">
+                    <i className="fa-brands fa-facebook text-xl"></i>
+
+                    <a href="" target="_blank" rel="noreferrer" className="link link-hover">Facebook</a>
                 </div>
-                <div className="flex items-center gap-2">
-                    <i className="fa-brands fa-whatsapp text-2xl"></i>
-                    <a className="link link-hover">Whatsapp</a>
+                <div className="flex items-center gap-2 hover:text-green-500 transition">
+                    <i className="fa-brands fa-whatsapp text-xl"></i>
+
+                    <a href="" target="_blank" rel="noreferrer" className="link link-hover">Whatsapp</a>
                 </div>
-                <div className="flex items-center gap-2">
-                    <i className="fa-brands fa-instagram text-2xl"></i>
-                    <a className="link link-hover">Instagram</a>
+                <div className="flex items-center gap-2 hover:text-pink-500 transition">
+                    <i className="fa-brands fa-instagram text-xl"></i>
+                    <a href="https://instagram.com" target="_blank" rel="noreferrer" className="link link-hover">Instagram</a>
                 </div>
-               
             </nav>
+
+
             <nav className="space-y-4">
                 <h6 className="footer-title">SERVICES</h6>
-                <a className="link link-hover">Branding</a>
-                <a className="link link-hover">Design</a>
-                <a className="link link-hover">Marketing</a>
-                <a className="link link-hover">Advertisement</a>
+                <span className="text-gray-400 text-sm">Vente de téléphones</span>
+                <span className="text-gray-400 text-sm">Accessoires d'origine</span>
+                <span className="text-gray-400 text-sm">Service Après-Vente (SAV)</span>
+                <span className="text-gray-400 text-sm">Livraison rapide</span>
             </nav>
+
+
             <nav className="space-y-4">
-                <h6 className="footer-title">COMPANY</h6>
-                <a className="link link-hover">Home</a>
-                <a className="link link-hover">Contact</a>
-                <a className="link link-hover">Information personnelles</a>
+                <h6 className="footer-title">LIENS UTILES</h6>
+                <Link to={"/"} className="link link-hover">Acceuil</Link>
+                <Link to={"/liste-produits"} className="link link-hover">Produit</Link>
+                {localStorage.getItem('token') ? (
+                    <Link to="/profil" className="link link-hover">Informations personnelles</Link>
+                ) : (
+                    <Link to="/login" className="link link-hover">Informations personnelles</Link>
+                )}
             </nav>
+
+
             <nav className="space-y-4">
                 <h6 className="footer-title">NOTRE SOCIETE</h6>
-                <a className="link link-hover">Mydirthph SARL</a>
-                <a className="link link-hover">Lot XXXXXXXX</a>
-                <a className="link link-hover">N:12354687/</a>
+                <span className="font-bold text-white">MyphoneMdsarl</span>
+                <span className="text-gray-400 text-sm">Lot XXXXXXXX</span>
+                <span className="text-gray-400 text-xs tracking-wider">STAT: 12354687/XXX</span>
             </nav>
         </footer>
-    )
+    );
 }
+
 export default Footer;

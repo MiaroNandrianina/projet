fcae8b803ce1155397860d885998b469cd53c47a
MiaroNandrianina <myaronandrianina@gmail.com>
2026-05-18 15:14:43 +0300

    ref footer

--- Front-end/src/components/Layouts/Footer.jsx
+++ Front-end/src/components/Layouts/Footer.jsx
@@ -1,36 +1,36 @@
 function Footer() {
     return (
         <footer className="footer sm:footer-horizontal bg-neutral text-neutral-content p-10">
-            <nav>
+            <nav className="space-y-4">
                 <h6 className="footer-title">SUIVEZ NOUS</h6>
-                <div>
+                <div className="flex items-center gap-2">
                     <i className="fa-brands fa-facebook-f text-2xl"></i>
                     <a className="link link-hover">Facebook</a>
                 </div>
-                <div>
+                <div className="flex items-center gap-2">
                     <i className="fa-brands fa-whatsapp text-2xl"></i>
                     <a className="link link-hover">Whatsapp</a>
                 </div>
-                <div>
+                <div className="flex items-center gap-2">
                     <i className="fa-brands fa-instagram text-2xl"></i>
                     <a className="link link-hover">Instagram</a>
                 </div>
                
             </nav>
-            <nav>
+            <nav className="space-y-4">
                 <h6 className="footer-title">SERVICES</h6>
                 <a className="link link-hover">Branding</a>
                 <a className="link link-hover">Design</a>
                 <a className="link link-hover">Marketing</a>
                 <a className="link link-hover">Advertisement</a>
             </nav>
-            <nav>
+            <nav className="space-y-4">
                 <h6 className="footer-title">COMPANY</h6>
                 <a className="link link-hover">Home</a>
                 <a className="link link-hover">Contact</a>
                 <a className="link link-hover">Information personnelles</a>
             </nav>
-            <nav>
+            <nav className="space-y-4">
                 <h6 className="footer-title">NOTRE SOCIETE</h6>
                 <a className="link link-hover">Mydirthph SARL</a>
                 <a className="link link-hover">Lot XXXXXXXX</a>
